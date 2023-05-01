bf00d3a02034e111f97e0a57d0d4b4e9f0685f12
Yaroslav3947 <kogutyaroslav3947@gmail.com>
2023-05-01 22:20:16 +0300

    test

--- mainwindow.h
+++ mainwindow.h
@@ -1,20 +1,13 @@
-
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
-
-
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
@@ -23,4 +16,3 @@ private:
     Ui::MainWindow *ui;
 };
 
-#endif // MAINWINDOW_H
