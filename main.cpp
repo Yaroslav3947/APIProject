a8f11f5d7330eba06e9c47af7d80ebd3f984e1cd
Yaroslav <kogutyaroslav3947@gmail.com>
2023-05-02 22:52:50 +0300

    test users API request and parse to json

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
-
-#include "mainwindow.h"
-
-#include <QApplication>
-
+#include <QtNetwork>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QCoreApplication a(argc, argv);
+
+    QNetworkAccessManager manager;
+
+    QNetworkRequest request;
+    request.setUrl(QUrl("https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=5"));
+
+    QNetworkReply *reply = manager.get(request);
+    QEventLoop loop;
+    QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    loop.exec();
+
+    if (reply->error() == QNetworkReply::NoError)
+    {
+        QByteArray response = reply->readAll();
+
+        QFile file("C:/Users/Yaroslav/Documents/APITestProject/result_get.json");
+        QJsonDocument jsonResponse = QJsonDocument::fromJson(response);
+        if (file.open(QIODevice::WriteOnly)) {
+            qDebug()  << jsonResponse["users"][0]["email"].toString();
+            file.write(response);
+            file.close();
+        }
+    }
+    else {
+        qDebug() << "Error:" << reply->errorString();
+    }
+
+    reply->deleteLater();
+    return 0;
 }
